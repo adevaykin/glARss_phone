e54f61750c867abe2e1b90cc32da3c146f78b08b
Alexander Devaykin <xaxellx@gmail.com>
2016-04-16 16:21:37 +0200

    Implemented taking a photo.

--- app/src/main/java/glarss/face/phone/glarssphone/WelcomeActivity.java
+++ app/src/main/java/glarss/face/phone/glarssphone/WelcomeActivity.java
@@ -1,13 +1,46 @@
 package glarss.face.phone.glarssphone;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        initActions();
+    }
+
+    private void initActions() {
+        final Button next_button = (Button) findViewById(R.id.next_btn);
+        assert next_button != null;
+        next_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        final Button photo_button = (Button) findViewById(R.id.take_photo_btn);
+        assert photo_button != null;
+        photo_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
